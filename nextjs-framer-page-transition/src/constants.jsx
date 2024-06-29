b605bb1f35b0db27488619f3340557a9610442aa
Ashwin <ashwinsnair97@gmail.com>
2024-06-29 23:17:05 +0530

    RS and S op Text

--- nextjs-framer-page-transition/src/constants.jsx
+++ nextjs-framer-page-transition/src/constants.jsx
@@ -207,74 +207,176 @@ export const projects =
         id: "recipe",
         title: "Recipe Simulator",
         imageList: false,
-        before: [],
-        after: [],
-        description: (<>
-            <p>Very nice recipe. There is a homely feel to it!</p></>),
-        imgs: ["/imgs/recipe/1.png", "/imgs/recipe/2.png", "/imgs/recipe/3.png", "/imgs/recipe/4.png", "/imgs/recipe/5.png", "/imgs/recipe/6.png", "/imgs/recipe/7.png"],
-        overview: (
-            <>
-                <p>
-                    Yummmmmmyyyy
-                </p>
-                {/* <ul style={{ listStyleType: 'disc' }}>
-                    <li>Identifying Contract Risks</li>
-                    <li>Tracking Supplier Performance</li>
-                    <li>Assistance with Contract Negotiation</li>
-                    <li>Personalized visualization for different personas</li>
-                </ul>
-                <p>
-                    Being part of this innovative project has been invaluable for my growth as a professional.
+        before: [
+            {
+                header: "TOOL INFO",
+                description: (<>
+                    <p>Recipe Simulator enables the user to make changes to the raws components going into the recipe of a finished good or make changes to a raws component directly in the portfolio and simulate to see the effects on the raws carbon emissions. </p></>)
+            },
+            {
+                header: "THE NEED",
+                description: (<>
+                    <p>The <span className="font-bold">formulation team</span>  at Mars Pet Nutrition NA did not have a method to quantify the impact on <span className="font-bold">carbon emissions</span>  that they can potentially drive through <span className="font-bold">ingredient changes</span>  in recipes. </p></>)
+            },
+            {
+                header: "THE SOLUTION",
+                description: (<>
+                    <p>The Pawprint Recipe Footprint Modeler is a web app what-if scenario simulator that <span className="font-bold">estimates GHG emission impact</span> across the MARS PNNA portfolio when <span className="font-bold">a change is made in a recipe</span> of a product. The solution utilizes the same data foundation created for the Pawprint Sustainability report.</p></>)
+            },
+            {
+                header: "How does it help business in MARS?",
+                description: (<>
+                    <p>Specific opportunities to <span className="font-bold">reduce raws emissions</span> are identified by formulators through the Pawprint report, who then utilize this tool to <span className="font-bold">substitute high emission ingredients</span> with potential lower emission ones and simulate based on <span className="font-bold">forecasted demand</span> the <span className="font-bold">resultant GHG emissions</span>. This enables creation of better Climate Action Plans.</p></>)
+            },
+        ],
+        after: [
+            {
+                header: "Design- Driven Decisions",
+                description: (
+                    <>
+                        <p>FILTERS SCREEN :</p>
+                        <ul>
+                            <li>Grouping of filters= The major pain point from the user base was the presence of a lot of options on the first page itself.  It was clearly pointing in the direction of going through the Hick’s Law checklist. So the ways to deal with were as follows: </li>
+                            <ol>
+                            <li>Try to reduce the number of options</li>
+                            <li>Find a way to hide items</li>
+                            <li>If you can’t go with any of the above, try to put all the options in an easily skimmable order and make sure the items are familiar. 
 
-                </p>
-                <p>
-                    The dashboard has approximately 50 unique users with around 15 active daily users. Commercial team makes supplier retention decisions and contract renewals based on supplier performance insights. This growth necessitated significant improvements in the dashboard's usability, signaling the end of the MVP phase.
-                </p>
-                <p>
-                    We had to ensure consistency, scalability, and sustainability across various elements, such as modals, buttons, and different page approaches. We embarked on developing a robust design system, collaborating closely to create fully auto-layout components. Occasionally, we had to "detach" components, even though we preferred not to.
-                </p>
-                <p>
-                    When discussing design systems, inspirations like Polaris, Carbon, or Atlassian often come to mind. We drew inspiration from these systems, adapting elements such as naming conventions, structure, and color systems to suit our needs. Our goal was to use the design system to enhance efficiency and tailor it to our unique requirements.
-                </p> */}
-            </>
-        ),
+As you can see, we went ahead with the third one, as it was the only option that was most suitable as per the requirements from the user. We narrowed down all the filters and put them in separate buckets for better decision making. </li>
+                             </ol>
+                             
+                            <li>Nudge= Since the user base for this tool was mostly an elder age group (50+ age) who is majorly accustomed to the Excel and Power BI, introducing something that is entirely different in terms of look and feel, could have proven to be a drastic dip in usage, engagement and retention. To solve this problem, we introduced <span className="font-bold">Hotspots</span>- a minimalist UX element that is designed to capture a user’s attention without being intrusive or annoying. We wanted to give subtle hints/ signifiers (small cues or context changes) that can affect user’s decisions.</li>
+                        </ul>
+                        <p>SIMULATION SCREEN :</p>
+                        <ul>
+                        <li>Validation Rules= To reduce the friction and an extra manual effort of going through all the steps and then getting an error to let the user know about the incorrect values entered, we used the Anchoring Bias- Users rely heavily on the first piece of information they see.   
+                        It is useful to increase the perceived value. </li>
+                            <li>Disclaimer in Simulation Page= Before the users click on Run Simulation to see the impact, we take a kind of double confirmation from them to check if they did make changes in the recipe (which is mandatory to do for at least one recipe). This is pertaining to the <span className="font-bold">reactance</span>- a UX principle, psychology related to user behavior, where people tend to react indifferently when they are taken away from their choices or range of options. But we also kept in mind that it doesn’t incline towards dark pattern. </li>
+                        </ul>
+                        <p>IMPACT SCREEN :</p>
+                        <ul>
+                        <li>Color coded accordions= The tree/ hierarchy chart and the cards showing the impact for the respective sections as used in the filters, have the same colors, hence incorporating <span className="font-bold">‘Law of Similarity’</span>- users perceive relationship between the elements that look similar. This helps the users to connect the related elements and their insights quickly without much of a mental hassle. It also considers another UX principle as well which is <span className="font-bold">‘Discoverability’</span>- The ease with which users can discover the features of the tool. It means that the user can extract the knowledge they need of the product by looking at it. If the key elements to take action are well presented then discoverability is good.  </li>
+                            <li>Floating Action Buttons= We have provided the users with numerous options as download excel, download the slides in Powerpoint and save the simulation results and metrics. Since one of the use cases involve multi selection, we kept a FAB at the mid bottom of the screen, for displaying the selected filters. </li>
+                        </ul>
+                    </>
+                )
+
+            },
+            {
+                header: "Other UI/ UX incorporations:",
+                description: (
+                    <>
+                        <ul>
+                            <li>Tutorials at the top left corner of each page= We adopted <span className="font-bold">‘Priming’</span> as previous stimuli influence user’s decisions, by giving subtle visual or verbal suggestions in the form of tutorials (containing a guide to use the tool throughout, which is a collection of the steps, screenshots and GIFs), helping users recall specific information, influencing how they respond. Priming works by activating an association or representation in users short- term memory just before another stimulus or task is introduced. </li>
+                            <li>Validation Rules= To reduce the <span className="font-bold">‘Cognitive Load’</span>- total amount of mental effort that is required to complete a task, or basically the processing power needed by the users to interact with a product, we gave the validation rules for the user to go through, before proceeding into the next steps, hence reducing the friction in terms of the overall experience. </li>
+                            <li>UOM in headers= We provided the units of measurement in the table headers to give  a better sense of the information to the users- this is was a part of the feedback survey that we did on current sets of users and also a bunch of non-users (Please note that this survey was possible as we had the luxury of time!). </li>
+                            <li>Loader Comments= Since a huge amount of data is responsible for the functioning of this tool, it takes time to load the screens. So, we introduced fun facts about the brand and the tool, to make the wait time not dip the user’s attention, hence increasing the engagement and the retention of the users. </li>
+                        </ul>
+                    </>
+                )
+
+            },
+        ],
+        imgs: ["/imgs/recipe/1.png", "/imgs/recipe/2.png", "/imgs/recipe/3.png", "/imgs/recipe/4.png", "/imgs/recipe/5.png", "/imgs/recipe/6.png", "/imgs/recipe/7.png"],
     },
 
     "sop_cockpit": {
         id: "sop_cockpit",
         title: "S&OP Cockpit",
         imageList: false,
-        before: [],
-        after: [],
-        description: (<>
-            <p>This also should be good. I guess?</p></>),
-        imgs: ["/imgs/sop_cockpit/1.png", "/imgs/sop_cockpit/2.png", "/imgs/sop_cockpit/3.png", "/imgs/sop_cockpit/4.png", "/imgs/sop_cockpit/5.png"],
-        overview: (
-            <>
-                <p>
-                    lets see what to put here
-                </p>
-                {/* <ul style={{ listStyleType: 'disc' }}>
-                    <li>Identifying Contract Risks</li>
-                    <li>Tracking Supplier Performance</li>
-                    <li>Assistance with Contract Negotiation</li>
-                    <li>Personalized visualization for different personas</li>
-                </ul>
-                <p>
-                    Being part of this innovative project has been invaluable for my growth as a professional.
+        before: [
+            {
+                header: "OVERVIEW",
+                description: (
+                    <>
+                    <p>(Will be giving later)</p>
+                        {/* <p>
+                       <span className="font-bold"> User Flow:</span> Elevating the user journey with meticulous attention to detail, creating a crafted experience that offers customized flows for streamlined interaction and comprehension.
+                        </p>
+                        <p>
+                       <span className="font-bold"> Intuitiveness: </span> Enhancing usability by simplifying navigation through an intuitive layout, ensuring a seamless and user-friendly experience.
+                        </p>
+                        <p>
+                       <span className="font-bold"> Report Responsiveness: </span> Improving operational efficiency by reducing wait times, ensuring prompt responses to user actions, and delivering timely and accurate reports.
+                        </p> */}
+                    </>
+                )
 
-                </p>
-                <p>
-                    The dashboard has approximately 50 unique users with around 15 active daily users. Commercial team makes supplier retention decisions and contract renewals based on supplier performance insights. This growth necessitated significant improvements in the dashboard's usability, signaling the end of the MVP phase.
-                </p>
-                <p>
-                    We had to ensure consistency, scalability, and sustainability across various elements, such as modals, buttons, and different page approaches. We embarked on developing a robust design system, collaborating closely to create fully auto-layout components. Occasionally, we had to "detach" components, even though we preferred not to.
-                </p>
-                <p>
-                    When discussing design systems, inspirations like Polaris, Carbon, or Atlassian often come to mind. We drew inspiration from these systems, adapting elements such as naming conventions, structure, and color systems to suit our needs. Our goal was to use the design system to enhance efficiency and tailor it to our unique requirements.
-                </p> */}
-            </>
-        ),
+            },
+            {
+                header: "Proposed Solution",
+                description: (
+                    <>
+                        <p>Reporting Pages:</p>
+                        <ul>
+                            <li>Intelligent findings panel- highlighting the KPIs’ deviation against target values</li>
+                            <li>On click KPI details breakdown</li>
+                            <li>Dynamic arrangement of KPIs along with newly added ones</li>
+                            <li>Objectives as new section</li>
+                            <li>All KPIs visible at a time on a same page without any manual scroll</li>
+                            <li>KPI search functionality</li>
+                        </ul>
+                        <p>Admin Page:</p>
+                        <ul>
+                        <li>Intelligent findings panel- highlighting the KPIs’ deviation against target values</li>
+                            <li>Create/ Modify/ Delete KPIs (single or multi-select) functionality</li>
+                            <li>Ability to change KPI names</li>
+                            <li>Manage user access to the application</li>
+                            <li>Change user roles without deleting the user details multiple times</li>
+                            <li>Extract excel data for all KPIs</li>
+                            <li>Extract data for active user details</li>
+                            <li>Download PPT in graphical format</li>
+                            <li>View all escalation cards across each segment</li>
+                        </ul>
+                    </>
+                )
+
+            },
+            {
+                header: "Non Functional Requirements",
+                description: (
+                    <>
+                        <ul>
+                            <li>Increase responsiveness of the tool</li>
+                            <li>Enhancement of the user experience</li>
+                            <li>Adaptability on mobile devices</li>
+                        </ul>
+                    </>
+                )
+
+            },
+        ],
+        after: [{
+            header: "Pain points addressed",
+            description: (
+                <>
+                    <ul>
+                        <li><span className="font-bold"> User Interaction and Experience:</span> Redesigned the tool to optimize user flows by leveraging cutting- edge design principles to elevate overall user experience of the tool.</li>
+                        <li><span className="font-bold"> Mobile device adaptability:</span> Accommodating different screen resolutions and aspect ratios by modularised code base and adaptive design.</li>
+                        <li><span className="font-bold"> Tool Responsiveness:</span> By implementing various enhancements on the frontend and backend part, tool’s response time reduced from 40 seconds to 7 seconds.</li>
+                    </ul>
+                </>
+            )
+
+        },
+        {
+            header: "UI/ UX principles to enable better consumption",
+            description: (
+                <>
+                    <ul>
+                    <li><span className="font-bold"> User Centric Design:</span> Design and structure of the tool to be highly intuitive for the user needs</li>
+                    <li><span className="font-bold"> Visual Hierarchy:</span> Clear structure including separation between sections to ensure an intuitive interface</li>
+                    <li><span className="font-bold"> Minimalism with a purpose:</span> High emphasis on decluttering and simplifying the user interface for better user experience</li>
+                    <li><span className="font-bold"> Consistent Design Language:</span> Consistency in colors, typography and layouts fostering familiarity</li>
+                    </ul>
+                </>
+            )
+
+        },
+    
+    ],
+        imgs: ["/imgs/sop_cockpit/1.png", "/imgs/sop_cockpit/2.png", "/imgs/sop_cockpit/3.png", "/imgs/sop_cockpit/4.png", "/imgs/sop_cockpit/5.png"],
+        
     },
     "ott": {
         id: "ott",
